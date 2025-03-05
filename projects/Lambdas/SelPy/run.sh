e0efab9931cc6ba4fa7e69ef01a0e9f706f6ad51
Hanubindh <42703510+hanubindh@users.noreply.github.com>
2025-03-05 23:16:23 +0000

    Update run.sh

--- projects/Lambdas/SelPy/run.sh
+++ projects/Lambdas/SelPy/run.sh
@@ -1,6 +1,7 @@
 set -euo pipefail
-rm -rf SelPy && \
-  S_PATH="https://raw.githubusercontent.com/hanubindh/hanubindh.github.io/refs/heads/master/projects/Lambdas/SelPy/run.sh" && \
-  mkdir SelPy && cd SelPy && \
-  curl -s ${S_PATH} | openssl base64 -d | tar -zxv && \
-  chmod +x deploy.sh && ./deploy.sh && cd .. && rm -rf SelPy'
+S_PATH="https://raw.githubusercontent.com/hanubindh/hanubindh.github.io/refs/heads/master/projects/Lambdas/SelPy/run.sh"
+T_DIR="SelPy"
+rm -rf ${T_DIR} && \
+  mkdir ${T_DIR} && cd ${T_DIR} && \
+  curl -s "${S_PATH}" | openssl base64 -d | tar -zxv && \
+  chmod +x deploy.sh && ./deploy.sh && cd .. && rm -rf ${T_DIR}'
